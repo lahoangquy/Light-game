d290ce16bc2ee5c163b908575787d5f32ed674cb
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2020-12-09 10:08:14 +0930

    Add files via upload

---
+++ build/precache-manifest.3119115da95978600f273c9a7fe91a08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c2523a4d82a2a936be2",
+    "url": "/Light-game/static/css/main.1adae553.chunk.css"
+  },
+  {
+    "revision": "0c2523a4d82a2a936be2",
+    "url": "/Light-game/static/js/main.0c2523a4.chunk.js"
+  },
+  {
+    "revision": "024dd0e473331f56a349",
+    "url": "/Light-game/static/js/1.024dd0e4.chunk.js"
+  },
+  {
+    "revision": "1760f0b041d5ea92f481",
+    "url": "/Light-game/static/js/runtime~main.1760f0b0.js"
+  },
+  {
+    "revision": "20c84402d0b36aac7da288c104098e69",
+    "url": "/Light-game/index.html"
+  }
+];
